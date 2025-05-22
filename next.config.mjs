c7f25bed6295e22d45dea7c4935f7cd0280c536c
DeveloperTariq786 <shaheendeveloper12@gmail.com>
2025-05-22 14:34:31 +0530

    adding the public API_Url2.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    NEXT_PUBLIC_API_URL: 'http://localhost:5000/api', // Adjust as needed for production
-  },
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
